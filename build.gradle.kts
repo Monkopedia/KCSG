b9e451c79ef0bbcc19b08507edd423bb4edf86b2
Jason Monk <monkopedia@gmail.com>
2022-06-25 15:08:20 -0400

    Update to latest gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("application")
     id("java")
     id("org.openjfx.javafxplugin") version "0.0.7"
+    kotlin("jvm") version "1.7.0"
 }
 
 application {
@@ -26,12 +29,21 @@ repositories {
 
 dependencies {
     
-    testCompile(group= "junit", name= "junit", version= "4.+")
+    testImplementation(group= "junit", name= "junit", version= "4.+")
 
    // compile group: "eu.mihosoft.ext.org.fxyz", name: "extfxyz", version: "0.4"
     //compile group: "eu.mihosoft.ext.org.fxyz", name: "extfxyz", version: "0.4", classifier: "sources"
-    compile(group= "eu.mihosoft.vvecmath", name= "vvecmath", version= "0.3.8")
-    compile(group= "eu.mihosoft.vvecmath", name= "vvecmath", version= "0.3.8", classifier= "sources")
-    compile("org.slf4j:slf4j-simple:1.6.1")
+    implementation(group= "eu.mihosoft.vvecmath", name= "vvecmath", version= "0.3.8")
+    implementation(group= "eu.mihosoft.vvecmath", name= "vvecmath", version= "0.3.8", classifier= "sources")
+    implementation("org.slf4j:slf4j-simple:1.6.1")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
