51e3b4b5aaaca887666f79582b843f825e9f3ac0
Jason Monk <monkopedia@gmail.com>
2025-04-06 15:18:29 -0400

    Update dependencies to latest

--- csgs/src/main/kotlin/Main.kt
+++ csgs/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 package com.monkopedia.csgs
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.flag
